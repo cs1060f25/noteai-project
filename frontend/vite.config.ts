ccefb7c471577993cf95dddb94b74fafc550e719
Atlani Eliot <eliot.atlani@student-cs.fr>
2025-11-06 16:22:28 -0500

    Fix google One Tap

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,4 +20,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    headers: {
+      'Referrer-Policy': 'no-referrer-when-downgrade',
+    },
+  },
 });
